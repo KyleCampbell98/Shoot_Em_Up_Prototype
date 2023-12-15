ea52286d7e135036789b58c498ad300142c3bc38
Kyle Campbell <kylereececampbell@gmail.com>
2023-12-15 00:00:09 +0000

    Player_Shape_Cutter Iterator
    Created iteration psudo code to be refactored.
    
    Cycles through the enums using an iterator that works off of the count of items in the ShapeCutter Enum. (Will help with scaling later on if more shapes are added to the game, better than a hard coded value).

--- Assets/Scripts/Player_Shape_Cutter.cs
+++ Assets/Scripts/Player_Shape_Cutter.cs
@@ -1,18 +1,32 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Player_Shape_Cutter : MonoBehaviour
 {
-    [SerializeField] private Cutter_And_Enemy_Shape_Enums.ShapeType selectedCutOutShape;
+    private Cutter_And_Enemy_Shape_Enums.ShapeType selectedShape;
+    [SerializeField] private Cutter_And_Enemy_Shape_Enums.ShapeType SelectedShape { get { return selectedShape; } set { selectedShape = value; } }
+    
+    [SerializeField] private int arrayIterator = 0;
 
+    private void Awake()
+    {
+       
+        SelectedShape = Cutter_And_Enemy_Shape_Enums.ShapeType.Circle ;
+    }
 
     void OnCycleShapes()
     {
-        Debug.Log("Shape cycling detected");
-        var values = Enum.GetValues(typeof(Cutter_And_Enemy_Shape_Enums.ShapeType));
-        Debug.Log($"Enum value printed: {values.GetValue(0).ToString()}");
+
+        if (arrayIterator >= Enum.GetNames(typeof(Cutter_And_Enemy_Shape_Enums.ShapeType)).Length)
+        {
+            arrayIterator = 0;
+        }
+       else arrayIterator++;
+        // Link below is for reading further into Getting the total number of values in an ENUM/ how ENUMS work. 
+        Debug.Log($"No of Enum value printed: {Enum.GetNames(typeof(Cutter_And_Enemy_Shape_Enums.ShapeType)).Length}"); // https://stackoverflow.com/questions/856154/total-number-of-items-defined-in-an-enum
     }
 
 
