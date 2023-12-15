d3ff05a5184dc046e943a86d0e70ba839b1ab14b
Kyle Campbell <kylereececampbell@gmail.com>
2023-12-15 16:53:24 +0000

    Assign Shape/Iterator
    Psudo code for shape assignment added in the form of a switch case. Needs to be refactored.
    
    Warning added if there isn't a switch case that corresponds to the number of potential shapes the player could choose.

--- Assets/Scripts/Player_Shape_Cutter.cs
+++ Assets/Scripts/Player_Shape_Cutter.cs
@@ -10,23 +10,48 @@ public class Player_Shape_Cutter : MonoBehaviour
     [SerializeField] private Cutter_And_Enemy_Shape_Enums.ShapeType SelectedShape { get { return selectedShape; } set { selectedShape = value; } }
     
     [SerializeField] private int arrayIterator = 0;
+    private int numberOfPossibleShapes;
 
     private void Awake()
     {
        
-        SelectedShape = Cutter_And_Enemy_Shape_Enums.ShapeType.Circle ;
+        SelectedShape = Cutter_And_Enemy_Shape_Enums.ShapeType.Circle;
+        Debug.Log($"Selected shape: {SelectedShape}");
+
+        numberOfPossibleShapes = Enum.GetNames(typeof(Cutter_And_Enemy_Shape_Enums.ShapeType)).Length;
+        Debug.Log($"Number of possible shapes: {numberOfPossibleShapes}");
     }
 
     void OnCycleShapes()
     {
+        // Link below is for reading further into Getting the total number of values in an ENUM/ how ENUMS work.
+        // https://stackoverflow.com/questions/856154/total-number-of-items-defined-in-an-enum
 
-        if (arrayIterator >= Enum.GetNames(typeof(Cutter_And_Enemy_Shape_Enums.ShapeType)).Length)
+        if (arrayIterator == numberOfPossibleShapes - 1)
         {
             arrayIterator = 0;
         }
-       else arrayIterator++;
-        // Link below is for reading further into Getting the total number of values in an ENUM/ how ENUMS work. 
-        Debug.Log($"No of Enum value printed: {Enum.GetNames(typeof(Cutter_And_Enemy_Shape_Enums.ShapeType)).Length}"); // https://stackoverflow.com/questions/856154/total-number-of-items-defined-in-an-enum
+        else { arrayIterator++; }
+        Debug.Log($"Iterator value is now: {arrayIterator}");
+        switch (arrayIterator)
+        {
+            case 0:
+                SelectedShape = Cutter_And_Enemy_Shape_Enums.ShapeType.Circle;
+                break;
+
+                case 1:
+                SelectedShape = Cutter_And_Enemy_Shape_Enums.ShapeType.Square;
+                break;
+
+                case 2:
+                SelectedShape = Cutter_And_Enemy_Shape_Enums.ShapeType.Triangle;
+                break;
+
+            default:
+                Debug.LogWarning("Enum list contains more values than shape selector switch case has scenarios for. UPDATE \"OnCycleShapes\" method.");
+                break;
+        }
+        Debug.Log($"Selected shape: {SelectedShape}");
     }
 
 
