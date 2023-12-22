51800adfb51c3db6b6a54f6fe56ae4ca5e184fdf
Kyle Campbell <kylereececampbell@gmail.com>
2023-12-22 17:48:09 +0000

    Player Projectile Planning
    Mapped out the script structure and some basic logic inclusions to solve problems such as timers as to which the player can be hurt after.

--- Assets/Player_Shape_Projectile_Logic.cs
+++ Assets/Player_Shape_Projectile_Logic.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class Player_Shape_Projectile_Logic : MonoBehaviour
 {
-    
+    // Object Control Attributes
+    private Cutter_And_Enemy_Shape_Enums.ShapeType? thisProjectilesShapeType; // sets shape type on each activation from the pool.
+    private bool canGameOverPlayer; // Should be false upon placing the projectile, could be done through a trigger behaviour, or a timer?
+
+    // Component References
+    private SpriteRenderer projectileSpriteRenderer; // Used to set sprite on each activation from the pool. 
+
+    // Timer attributes
+    private float timeBeforeActivatingDanger; // This is the amount of seconds after being placed that the danger to the player of their own projectile will be instated.
+
+    // Object Control Properties
+    public Cutter_And_Enemy_Shape_Enums.ShapeType? ProjectilesShapeType { get { return thisProjectilesShapeType; } 
+        set 
+        { // Need To ensure this is only called on the projectile's activation. 
+            thisProjectilesShapeType = value; 
+        } 
+    }
+
+    private void OnDisable()
+    {
+        ResetProjectile();
+    }
+
+    private void ResetProjectile()
+    {
+        projectileSpriteRenderer.sprite = null;
+
+    }
 }
