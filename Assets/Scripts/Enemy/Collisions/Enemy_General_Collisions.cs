3de57c6d3335f1ab20d3f947d0d5504ea097c3aa
Kyle Campbell <kylereececampbell@gmail.com>
2024-01-11 14:49:37 +0000

    GITHUB_BRANCH_TEST
    Just added a simple comment. This is simply a test to test merging and pull requests in this project.

--- Assets/Scripts/Enemy/Collisions/Enemy_General_Collisions.cs
+++ Assets/Scripts/Enemy/Collisions/Enemy_General_Collisions.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Enemy_General_Collisions : MonoBehaviour
 {
+    /// <summary>
+    ///  TESTING FIRST GITHUB BRANCH
+    /// </summary>
+
     // Start is called before the first frame update
     void Start()
     {
