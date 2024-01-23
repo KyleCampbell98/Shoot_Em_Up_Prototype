589dd79c26a5024b2eaff91a42fd286757eda154
Kyle Campbell <kylereececampbell@gmail.com>
2024-01-23 22:12:33 +0000

    Linked Up Some UI
    - Last day on break away from work/ development
    
    - Just to keep up some project interaction, linked up UI fields to scriptable object data.

---
+++ Assets/UI_Scoring_Control.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+
+public class UI_Scoring_Control : MonoBehaviour
+{
+    [Header("Scoring Cached Storage")]
+    [SerializeField] private InPlay_Details scoringCache;
+
+    [Header("UI Element References")]
+    [SerializeField] private TextMeshProUGUI bestTimeSurvived_TMP;
+    [SerializeField] private TextMeshProUGUI bestEnemiesDefeated_TMP;
+
+    private void Start()
+    {
+        Debug.Log("Scene Load call test");
+        bestEnemiesDefeated_TMP.text = scoringCache.BestEnemiesDefeated.ToString();
+        bestTimeSurvived_TMP.text = scoringCache.BestSurvivalTime.ToString();
+
+    }
+
+
+}
