b85eade312580a2123fb97a437403ca0b4182acf
Kyle Campbell <kylereececampbell@gmail.com>
2024-01-26 16:50:41 +0000

    Began sprite change on player damage
    - When player takes damage, different sprites are assigned to indicate damage level, not yet working properly due to indexing issues, and issues in the amount of times a collision happens on contact with an enemy.
    -Worth implementing a "invulnerability" window once player gets hit to give leeway and avoid rapid successive hits to the player.

--- Assets/Scripts/Player/Player_Damage_Control.cs
+++ Assets/Scripts/Player/Player_Damage_Control.cs
@@ -8,6 +8,7 @@ public class Player_Damage_Control : MonoBehaviour
     [Header("Reference Cache")]
     [SerializeField] private Transform playerTransform;
     [SerializeField] private SpriteRenderer playerSpriteRenderer;
+    [SerializeField] private Sprite[] healthStateSprites;
     
     [Header("Player Stats")]
     [SerializeField] private int playerHP;
@@ -17,8 +18,10 @@ public class Player_Damage_Control : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        PlayerHP = healthStateSprites.Length;
+        Array.Reverse(healthStateSprites);
        playerSpriteRenderer = Static_Helper_Methods.FindComponentInGameObject<SpriteRenderer>(this.gameObject);
-        
+        Player_Collisions.playerTookDamage += PlayerTookDamage;
     }
 
 
@@ -30,8 +33,16 @@ public class Player_Damage_Control : MonoBehaviour
         Debug.Log("OnPlayerHit Called from game manager script");
         if ((PlayerHP - 1) <= 0) { /* Game End Scenario Code */ }
         PlayerHP--;
+        PlayerHealthStateSprite();
+
+
     }
 
+    private void PlayerHealthStateSprite()
+    {
+      // Need to implement a check for the player being dead so that the index doesnt go out of bounds.
+     playerSpriteRenderer.sprite = healthStateSprites[playerHP];
+    }
     
 
 }
