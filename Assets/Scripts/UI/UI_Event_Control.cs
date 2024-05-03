8ecfdacc57e55ac18c2f7175ca13757320fdb897
Kyle Campbell <kylereececampbell@gmail.com>
2024-05-04 00:31:13 +0100

    Screen Focus Control Implemented
    When the screen is clicked off of, then clicked back onto, the last selected UI object is resumed as the current UI selection.

--- Assets/Scripts/UI/UI_Event_Control.cs
+++ Assets/Scripts/UI/UI_Event_Control.cs
@@ -6,16 +6,34 @@ using UnityEngine.EventSystems;
 
 public class UI_Event_Control : MonoBehaviour
 {
-    [SerializeField] private GameObject initialButtonSelection;
-
+    [SerializeField] protected GameObject initialButtonSelection;
+    private GameObject lastSelectedObjectBeforeFocusLoss;
     // Start is called before the first frame update
-    void Start()
+    protected void Start()
     {
+        Debug.Log("Event system start called");
         SetInitialButtonSelection();
     }
 
-    private void SetInitialButtonSelection()
+    protected void OnApplicationFocus(bool focus)
     {
+        if(focus) 
+        {
+            Debug.Log("Focus to app returned");
+          
+            EventSystem.current.SetSelectedGameObject(lastSelectedObjectBeforeFocusLoss); 
+            
+        }
+        else
+        {
+            lastSelectedObjectBeforeFocusLoss = EventSystem.current.currentSelectedGameObject;
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+    }
+
+    protected void SetInitialButtonSelection()
+    {
+        Debug.Log("Initial Button Selection Called");
         EventSystem.current.SetSelectedGameObject(null); // Good practice to set null before resetting to anything else;
         EventSystem.current.SetSelectedGameObject(initialButtonSelection);
     }
